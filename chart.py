f4847a19fab95b56440f7ff514f72f84f0458e4f
Karin Kato <kkato1@charlotte.edu>
2025-02-12 18:05:39 -0500

    Update chart.py
    Final

--- chart.py
+++ chart.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as pylt
 
 data = [
     ("Time taken to sort 10 elements:", 4.839e-06),
-    ("Time taken to sort 10 elements:", 4.5972e-05),
-    ("Time taken to sort 10 elements:", 0.00050224),
-    ("Time taken to sort 10 elements:", 0.0056795),
-    ("Time taken to sort 10 elements:", 0.0629707),
-    ("Time taken to sort 10 elements:", 0.67559),
-    ("Time taken to sort 10 elements:", 7.21877),
-    ("Time taken to sort 10 elements:", 77.049),
-    ("Time taken to sort 10 elements:", 834.524)
+    ("Time taken to sort 100 elements:", 4.5972e-05),
+    ("Time taken to sort 1000 elements:", 0.00050224),
+    ("Time taken to sort 10000 elements:", 0.0056795),
+    ("Time taken to sort 100000 elements:", 0.0629707),
+    ("Time taken to sort 1000000 elements:", 0.67559),
+    ("Time taken to sort 10000000 elements:", 7.21877),
+    ("Time taken to sort 100000000 elements:", 77.049),
+    ("Time taken to sort 1000000000 elements:", 834.524)
 ]
 
 d = panda.DataFrame(data, columns=["Description", "Time"])
